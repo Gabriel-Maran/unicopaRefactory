6a97f4c58e70b0ed7799a961be2938662c089dda
Gabriel-Maran <gabriel.enzo.libero.maran@gmail.com>
2026-05-05 20:28:14 -0300

    error: removendo funcao

--- app/utils/GroupGames.js
+++ app/utils/GroupGames.js
@@ -16,26 +16,6 @@ export function groupGameByDate(jogos) {
   });
 }
 
-export function groupGameByDate(jogos, grupo) {
-  const jogosAgrupados = jogos.reduce((acc, jogo) => {
-    const data = jogo.data_brasilia;
-    if (jogo.grupo.toLowerCase() === grupo.toLowerCase()) {
-      if (!acc[data]) {
-        acc[data] = [];
-      }
-      acc[data].push(jogo);
-      return acc;
-    }
-  }, {});
-
-  return Object.keys(jogosAgrupados).map((data) => {
-    return {
-      title: data,
-      data: sortGameByHorario(jogosAgrupados[data]),
-    };
-  });
-}
-
 function sortGameByHorario(jogosDia) {
   return jogosDia.sort(
     (a, b) => paraMinutos(a.hora_brasilia) - paraMinutos(b.hora_brasilia),
